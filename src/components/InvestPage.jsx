f6910b46b044b26e00fde9952021ad31597fcff8
ANKIT ARJUN KUSHWAHA <ankitkushwaha.it@gmail.com>
2026-05-18 12:12:21 +0530

    Replace investment links with placeholders
    Updated investment options to include placeholders for links.

--- src/components/InvestPage.jsx
+++ src/components/InvestPage.jsx
@@ -2,17 +2,20 @@ const investOptions = [
   {
     title: 'Mutual Fund',
     text: 'Explore long-term market linked investment plans.',
-    link: 'https://investmentlife.policybazaar.com/prequote-ulip?offerid=37&amount=9000&utm_source=google&utm_medium=cpc&utm_campaign=Investment_Domestic_PMax_South_India00ULIP&gad_source=1&gad_campaignid=23458665035&gbraid=0AAAAAD2hd25xgWDEwglA0F3GwQHYZTkCZ&gclid=Cj0KCQjwzqXQBhD2ARIsAKrIeU8CZCY3-kavf0W5q6TOXvT8bXDtReL3aWfuVCsHMScvGYpFfIVcZmgaAiUUEALw_wcB',
+    link: 
+      # required link
   },
   {
     title: 'Gold',
     text: 'Plan digital gold or physical gold savings.',
-    link: 'https://www.sundarammutual.com/sundaram-multi-asset-allocation-fund?SMAAF&utm_source=google&utm_medium=cpc&utm_campaign=SB_11_1_SMF_SC_LG_GE_T1&T2_Apr26&gad_source=1&gad_campaignid=23755781679&gbraid=0AAAAADiR9aVW94RNh3bZHDiK9dqGA4V1j&gclid=Cj0KCQjwzqXQBhD2ARIsAKrIeU8K4drLIudZl1cUYCXACFlTUrMGs8JoPpsb9CqUVhFZRqHXDg11K4YaAgEREALw_wcB',
+    link: 
+    # required link
   },
   {
     title: 'FD',
     text: 'Compare fixed deposit style safe returns.',
-    link: 'https://investmentlife.policybazaar.com/prequote-trad?offerid=2004&utm_content=home_v5&utm_source=google&utm_medium=cpc&utm_campaign=Guaranteed_Plan_Exact_Desktop00FD&utm_term=fixed%20deposit&gad_source=1&gad_campaignid=11580977907&gbraid=0AAAAAD2hd24DUkdKQDdYOlfr-ZTrLonDS&gclid=Cj0KCQjwzqXQBhD2ARIsAKrIeU_DpqmjKqBH_DLLqSSQvVgf9g_Ge7GIpnFAi-3X2h8Hk-f5HgB2x1UaAszTEALw_wcB',
+    link:
+    # required link    
   },
 ]
 
